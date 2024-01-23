484dbde6d0daa3004d030eb4eec8993d1d8b79a6
jaepang <jake_king@kakao.com>
2024-01-23 22:08:49 +0900

    default code format setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+  "printWidth": 160,
+  "semi": false,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "arrowParens": "always",
+  "bracketSameLine": false,
+  "singleAttributePerLine": true
+}
