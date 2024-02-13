22b3aa8769f31355ec54d6eb9f7a8958d5bf15a1
jaepang <jake_king@kakao.com>
2024-02-13 23:33:11 +0900

    create color schema for global color

---
+++ src/assets/colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  sora: '#374fff',
+  white: '#ffffff',
+  yellow: '#A7FF36',
+  grey: '#c8c8c8',
+  writtenGrey: '#363636',
+  bgGrey: '#eeeeee',
+  lightGrey: '#888888',
+  godong: '#AE704D',
+}
