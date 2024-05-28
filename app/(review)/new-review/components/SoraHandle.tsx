93b55796cc70587fe19608a99669f38aa6936e99
jaepang <jake_king@kakao.com>
2024-05-29 00:11:55 +0900

    implement sorahandle haptic feedback

--- app/(review)/new-review/components/SoraHandle.tsx
+++ app/(review)/new-review/components/SoraHandle.tsx
@@ -1,33 +1,50 @@
-import { useRef } from 'react'
-import { Animated, View, PanResponder, Vibration } from 'react-native'
+import { useRef, useState, useEffect } from 'react'
+import { Animated, View, PanResponder, type PanResponderInstance } from 'react-native'
 import { BgSora } from '@/assets/icons'
 import { Colors } from '@/assets/colors'
 import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics'
 
 export default function SoraHandle({ review, x, setX }: { review: string; x: number; setX: (x: number) => void }) {
+  const [panResponder, setPanResponder] = useState<PanResponderInstance>()
+  const lastHapticTime = useRef(Date.now())
   const pan = useRef(new Animated.ValueXY()).current
   const isReviewWritten = review?.length > 0
 
   pan.addListener((value) => {
     if (isReviewWritten) {
-      if (Math.abs(x) % 10 === 0) impactAsync(ImpactFeedbackStyle.Soft)
       setX(value.x)
     }
   })
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: Animated.event([null, { dx: pan.x }], { useNativeDriver: false }),
-      onPanResponderRelease: (_, state) => {
-        const submitReview = isReviewWritten && Math.abs(state.dx) > 150
-        const fActivated = state.dx > 150
-        const tActivated = state.dx < -150
+  useEffect(
+    () =>
+      setPanResponder(
+        PanResponder.create({
+          onMoveShouldSetPanResponder: () => true,
+          onPanResponderMove: Animated.event([null, { dx: pan.x }], {
+            useNativeDriver: false,
+            listener: (evt) => {
+              if (isReviewWritten) {
+                const now = Date.now()
+                if (now - lastHapticTime.current > 100) {
+                  // 100ms 마다 햅틱 피드백을 줍니다.
+                  impactAsync(ImpactFeedbackStyle.Light)
+                  lastHapticTime.current = now
+                }
+              }
+            },
+          }),
+          onPanResponderRelease: (_, state) => {
+            const submitReview = isReviewWritten && Math.abs(state.dx) > 150
+            const fActivated = state.dx > 150
+            const tActivated = state.dx < -150
 
-        Animated.spring(pan, { toValue: { x: 0, y: 0 }, useNativeDriver: false, speed: 6, bounciness: 0 }).start()
-      },
-    }),
-  ).current
+            Animated.spring(pan, { toValue: { x: 0, y: 0 }, useNativeDriver: false, speed: 6, bounciness: 0 }).start()
+          },
+        }),
+      ),
+    [isReviewWritten],
+  )
 
   return (
     <>
@@ -39,7 +56,7 @@ export default function SoraHandle({ review, x, setX }: { review: string; x: num
           alignItems: 'center',
           zIndex: 100,
         }}
-        {...panResponder.panHandlers}
+        {...panResponder?.panHandlers}
       >
         <View style={{ width: 20, aspectRatio: 1, borderRadius: 100, borderWidth: 3, borderColor: Colors.lightGrey }} />
         <View style={{ width: Math.abs(x), height: 3, backgroundColor: Colors.lightGrey }} />
