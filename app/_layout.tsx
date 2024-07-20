dce145590efc267b372fde47992fdd64f0ea217c
jaepang <jake_king@kakao.com>
2024-07-20 17:58:48 +0900

    add unique device id per device

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react'
 import { Stack } from 'expo-router/stack'
+import DeviceInfo from 'react-native-device-info'
 import { polyfill } from '@/utils'
 
 polyfill()
 
 export default function Layout() {
+  const [deviceId, setDeviceId] = useState<string | null>(null)
+
+  useEffect(() => {
+    async function getDeviceId() {
+      console.log('Getting device id')
+      const id = await DeviceInfo.getUniqueId()
+      setDeviceId(id)
+    }
+
+    getDeviceId()
+  }, [])
+
+  console.log(deviceId)
+
   return <Stack screenOptions={{ header: () => null }} />
 }
