5346f866768b6bd47e1f47dced094d986e10fa60
jaepang <jake_king@kakao.com>
2024-09-06 01:05:16 +0900

    fix placeholder

--- app/(review)/new-review/index.tsx
+++ app/(review)/new-review/index.tsx
@@ -7,7 +7,8 @@ import SoraReponseMenu from '@/components/common/SoraReponseMenu'
 import { useOpenAIStream } from '@/hooks'
 import { ReviewScreensNavbar } from '@/components'
 
-const PLACEHOLDER = '어떤 일이 있었나요? 무슨 느낌이나 생각이 들었나요?\n*하루에 한 번, 손잡이를 원하는 방향으로 잡아당겨 소라의 답변을 들을 수 있어요.'
+const PLACEHOLDER =
+  '오늘은 어떤 일이 있었나요?\n무슨 느낌이나 생각이 들었나요?\n(1000자 이내)\n\n*하루에 한 번, 손잡이를 원하는 방향으로 잡아당겨 소라의 답변을 들을 수 있어요.'
 
 export default function WriteReviewScreen() {
   const reviewContext = useReviewContext()
@@ -56,9 +57,10 @@ export default function WriteReviewScreen() {
 
 const style = StyleSheet.create({
   textInput: {
+    width: '86%',
     textAlignVertical: 'top',
-    paddingLeft: 20,
-    paddingRight: 20,
+    paddingLeft: '7%',
+    paddingRight: '7%',
     paddingBottom: 100,
     fontSize: 14,
     lineHeight: 30,
