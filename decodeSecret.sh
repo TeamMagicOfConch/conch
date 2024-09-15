d60e55e74be1a434bbab942d9a1b85c9a2b65d81
jaepang <jake_king@kakao.com>
2024-09-15 18:54:26 +0900

    add script for using google services api key

---
+++ decodeSecret.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create the directory if it doesn't exist
+mkdir -p ./googleServices
+
+# Decode the base64 strings and create the files
+echo $GOOGLE_SERVICES_JSON_BASE64 | base64 --decode > ./googleServices/google-services.json
