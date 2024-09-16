2945c21ee080e75497dae5efa22a2ed516232104
jaepang <jake_king@kakao.com>
2024-09-17 00:14:54 +0900

    remove using google key -> submitted on eas

--- decodeSecret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create the directory if it doesn't exist
-mkdir -p ./googleServices
-
-# Decode the base64 strings and create the files
-echo $GOOGLE_SERVICES_JSON_BASE64 | base64 --decode > ./googleServices/google-services.json
