d4fe232f6cf00186068c7751d74b3b1563f94105
jaepang <jake_king@kakao.com>
2024-10-02 20:14:03 +0900

    remove console.log

--- app/(app)/(calendar)/_layout.tsx
+++ app/(app)/(calendar)/_layout.tsx
@@ -2,7 +2,6 @@ import { SafeAreaViewWithDefaultBackgroundColor } from '@/components'
 import { Slot } from 'expo-router'
 
 export default function CalendarLayout() {
-  console.log('calendar layout')
   return (
     <SafeAreaViewWithDefaultBackgroundColor>
       <Slot />
--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from 'expo-router/stack'
 
 export default function AppLayout() {
-  console.log('app layout')
   return <Stack screenOptions={{ header: () => null }} />
 }
