361fae2ff78eb12fe3ccb3ca8cbc85e801d48779
jaepang <jake_king@kakao.com>
2024-10-06 20:24:03 +0900

    fasten auth refresh

--- utils/auth.ts
+++ utils/auth.ts
@@ -14,10 +14,15 @@ export function setTokens({ accessToken, refreshToken, rest }: { accessToken?: s
   ])
 }
 
-export async function refreshAuth() {
+const DEFAULT_TOKENS = {
+  accessToken: null,
+  refreshToken: null,
+}
+
+export async function refreshToken() {
   console.log('refresh auth')
   const _refreshToken = await AsyncStorage.getItem(consts.asyncStorageKey.refreshToken)
-  if (!_refreshToken) return null
+  if (!_refreshToken) return DEFAULT_TOKENS
 
   try {
     const refreshResponse = await fetch(`${process.env.EXPO_PUBLIC_TEMP_API_URL}/user/reissue`, {
@@ -29,19 +34,36 @@ export async function refreshAuth() {
     const {
       data: { accessToken, refreshToken },
     } = await refreshResponse.json()
-    await setTokens({ accessToken, refreshToken })
+    setTokens({ accessToken, refreshToken })
+
+    return { accessToken, refreshToken }
   } catch (e) {
     console.warn('reissue failed', e)
+    return DEFAULT_TOKENS
   }
 }
 
-export async function fetchWithAuth(url: string, options: RequestInit) {
-  const tokenExpiresAt = new Date((await AsyncStorage.getItem(consts.asyncStorageKey.tokenExpiresAt)) as string)
-  const tokenExpireExpected = tokenExpiresAt.getTime() < new Date().getTime()
-
-  if (tokenExpireExpected) await refreshAuth()
-
+export async function fetchWithAuth(url: string, _options?: RequestInit) {
   try {
+    const tokenExpiresAt = new Date((await AsyncStorage.getItem(consts.asyncStorageKey.tokenExpiresAt)) as string)
+    const tokenExpireExpected = tokenExpiresAt.getTime() < new Date().getTime()
+
+    const accessToken = tokenExpireExpected ? (await refreshToken())?.accessToken : await AsyncStorage.getItem(consts.asyncStorageKey.accessToken)
+
+    const options = {
+      ..._options,
+      headers: {
+        ..._options?.headers,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+
+    const response = await fetch(url, options)
+
+    if (response.status === 200) return response
+
+    const { accessToken: refreshedAccessToken } = await refreshToken()
+    return fetch(url, options)
   } catch (e) {
     console.warn('fetchWithAuth failed', e)
   }
